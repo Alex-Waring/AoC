39a3f99f23b2627bd039502bb4969e6a91a3f02e
Alex Waring <alex.waring@starlingbank.com>
2023-12-18 21:24:36 +0000

    use new location system

--- 2023/Day16/day16.go
+++ 2023/Day16/day16.go
@@ -13,13 +13,14 @@ type Location struct {
 	mirror    string
 }
 
-func part1(grid map[int]map[int]Location, start_y int, start_x int, start_dy int, start_dx int) int {
-	beams := [][]int{{start_y, start_x, start_dy, start_dx}}
+func part1(grid map[utils.Position]Location, start_y int, start_x int, start_dy int, start_dx int) int {
+	q := utils.Queue[[]int]{}
+	q.Push([]int{start_y, start_x, start_dy, start_dx})
 
 	seen := []string{}
 
-	for len(beams) > 0 {
-		beam := beams[0]
+	for !q.IsEmpty() {
+		beam := q.Pop()
 		y, x, dy, dx := beam[0], beam[1], beam[2], beam[3]
 
 		xx, yy := x+dx, y+dy
@@ -28,19 +29,18 @@ func part1(grid map[int]map[int]Location, start_y int, start_x int, start_dy int
 
 		// if we've seen it continue
 		if utils.StringInSlice(key, seen) {
-			beams = beams[1:]
 			continue
 		}
 
 		// if invalid coords continue
-		if !((0 <= yy && yy < len(grid)) && (0 <= xx && xx < len(grid[0]))) {
-			beams = beams[1:]
+		position := utils.NewPosition(yy, xx)
+		if _, exists := grid[position]; !exists {
 			continue
 		}
 
 		seen = append(seen, key)
 
-		switch tile := grid[yy][xx].mirror; tile {
+		switch tile := grid[position].mirror; tile {
 		case "/":
 			dx, dy = -dy, -dx
 		case `\`:
@@ -48,36 +48,32 @@ func part1(grid map[int]map[int]Location, start_y int, start_x int, start_dy int
 		case "|":
 			if dx == 1 || dx == -1 {
 				dx, dy = 0, 1
-				beams = append(beams, []int{yy, xx, -1, 0})
+				q.Push([]int{yy, xx, -1, 0})
 			}
 		case "-":
 			if dy == 1 || dy == -1 {
 				dx, dy = 1, 0
-				beams = append(beams, []int{yy, xx, 0, -1})
+				q.Push([]int{yy, xx, 0, -1})
 			}
 		}
 
-		beams = append(beams, []int{yy, xx, dy, dx})
-
-		beams = beams[1:]
+		q.Push([]int{yy, xx, dy, dx})
 	}
 
 	for _, value := range seen {
 		x, y := strings.Split(value, ",")[1], strings.Split(value, ",")[0]
 
-		location := grid[utils.IntegerOf(y)][utils.IntegerOf(x)]
-		location.energized = true
+		position := grid[utils.NewPosition(utils.IntegerOf(y), utils.IntegerOf(x))]
+		position.energized = true
 
-		grid[utils.IntegerOf(y)][utils.IntegerOf(x)] = location
+		grid[utils.NewPosition(utils.IntegerOf(y), utils.IntegerOf(x))] = position
 	}
 
 	total := 0
 
-	for y := 0; y < len(grid); y++ {
-		for x := 0; x < len(grid[0]); x++ {
-			if grid[y][x].energized {
-				total++
-			}
+	for _, position := range grid {
+		if position.energized {
+			total++
 		}
 	}
 	return total
@@ -86,14 +82,12 @@ func part1(grid map[int]map[int]Location, start_y int, start_x int, start_dy int
 func main() {
 	input := utils.ReadInput("input.txt")
 
-	grid := map[int]map[int]Location{}
+	grid := map[utils.Position]Location{}
 
 	for y, line := range input {
 		for x, charecter := range line {
-			if _, ok := grid[y]; !ok {
-				grid[y] = make(map[int]Location)
-			}
-			grid[y][x] = Location{mirror: string(charecter)}
+			poition := utils.NewPosition(y, x)
+			grid[poition] = Location{mirror: string(charecter)}
 		}
 	}
 
@@ -101,13 +95,16 @@ func main() {
 
 	part2_values := []int{}
 
-	for y := 0; y < len(grid); y++ {
+	no_of_rows := len(input)
+	no_of_cols := len(input[0])
+
+	for y := 0; y <= no_of_rows; y++ {
 		part2_values = append(part2_values, part1(grid, y, -1, 0, 1))
-		part2_values = append(part2_values, part1(grid, y, len(grid[0]), 0, -1))
+		part2_values = append(part2_values, part1(grid, y, no_of_cols, 0, -1))
 	}
-	for x := 0; x < len(grid[0]); x++ {
+	for x := 0; x <= no_of_cols; x++ {
 		part2_values = append(part2_values, part1(grid, -1, x, 1, 0))
-		part2_values = append(part2_values, part1(grid, len(grid), x, -1, 0))
+		part2_values = append(part2_values, part1(grid, no_of_rows, x, -1, 0))
 	}
 
 	fmt.Println(slices.Max(part2_values))
